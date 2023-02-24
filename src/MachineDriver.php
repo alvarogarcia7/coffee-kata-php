1dd2d9dbcbc7ca0a042aee9ffd34a229add497be
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 21:44:53 +0400

    green

--- src/MachineDriver.php
+++ src/MachineDriver.php
@@ -19,7 +19,13 @@ class MachineDriver
                 return "M:missing-money:{$missingMoney}";
             }
         } elseif ($request->drink === "coffee") {
-            return "C:2:0";
+            $teaPrice = 0.6;
+            if ($request->availableMoney >= $teaPrice) {
+                return "C:2:0";
+            } else {
+                $missingMoney = $teaPrice - $request->availableMoney;
+                return "M:missing-money:{$missingMoney}";
+            }
         }
         $teaPrice = 0.4;
         if ($request->availableMoney >= $teaPrice) {
