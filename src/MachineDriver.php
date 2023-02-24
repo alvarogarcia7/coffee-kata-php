8bb56c2738bdc5bd1d44c9b1f9d1a4f1a9037cb8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 21:45:14 +0400

    Refactor: rename variable (wrong name before)

--- src/MachineDriver.php
+++ src/MachineDriver.php
@@ -11,27 +11,27 @@ class MachineDriver
             return "M:{$request->message}";
         }
         if ($request->drink === "chocolate") {
-            $teaPrice = 0.5;
-            if ($request->availableMoney >= $teaPrice) {
+            $price = 0.5;
+            if ($request->availableMoney >= $price) {
                 return "H::";
             } else {
-                $missingMoney = $teaPrice - $request->availableMoney;
+                $missingMoney = $price - $request->availableMoney;
                 return "M:missing-money:{$missingMoney}";
             }
         } elseif ($request->drink === "coffee") {
-            $teaPrice = 0.6;
-            if ($request->availableMoney >= $teaPrice) {
+            $price = 0.6;
+            if ($request->availableMoney >= $price) {
                 return "C:2:0";
             } else {
-                $missingMoney = $teaPrice - $request->availableMoney;
+                $missingMoney = $price - $request->availableMoney;
                 return "M:missing-money:{$missingMoney}";
             }
         }
-        $teaPrice = 0.4;
-        if ($request->availableMoney >= $teaPrice) {
+        $price = 0.4;
+        if ($request->availableMoney >= $price) {
             return "T:1:0";
         } else {
-            $missingMoney = $teaPrice - $request->availableMoney;
+            $missingMoney = $price - $request->availableMoney;
             return "M:missing-money:{$missingMoney}";
         }
     }
