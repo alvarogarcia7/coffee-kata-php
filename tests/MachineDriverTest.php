6c445dc0c7f9de0d063e3b8d8497fd785f3428a6
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 21:37:04 +0400

    Red: not enough money to make a tea

--- tests/MachineDriverTest.php
+++ tests/MachineDriverTest.php
@@ -30,6 +30,7 @@ class MachineDriverTest extends TestCase
     {
         return [
             'With enough money, Drink maker makes 1 tea with 1 sugar and a stick' => [(new UserRequestBuilder())->tea()->withMoney(0.4)->withSugar()->build(), "T:1:0"],
+            'Without enough money, Drink maker makes 1 tea with 1 sugar and a stick' => [(new UserRequestBuilder())->tea()->withMoney(0.1)->withSugar()->build(), "M:missing-money:0.3"],
             'With enough money, Drink maker makes 1 chocolate with no sugar - and therefore no stick' => [(new UserRequestBuilder())->chocolate()->withMoney(0.6)->withoutSugar()->build(), "H::"],
             'With enough money, Drink maker makes 1 coffee with 2 sugars and a stick' => [(new UserRequestBuilder())->coffee()->withSugar()->withSugar()->withMoney(0.5)->build(), "C:2:0"],
             'With enough money, Drink maker forwards any message received onto the coffee machine interface for the customer to see' => [(new UserRequestBuilder())->printMessage("message-content")->build(), "M:message-content"],
