55dc2e78c647955cf137c3e9ad608c163c6524a7
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 22:13:31 +0400

    Refactor: move part of the construction of the Drink Command to the Drink (continue)

--- src/MachineDriver.php
+++ src/MachineDriver.php
@@ -14,7 +14,7 @@ class MachineDriver
         $this->validDrinks = [new Drink("tea", 0.4, true, ""),
             new Drink("coffee", 0.6, true, "C:2:0"),
             new Drink("chocolate", 0.5, true, "H::"),
-            new Drink("orangeJuice", 0.6, false, ""),
+            new Drink("orangeJuice", 0.6, false, "O::"),
         ];
     }
 
@@ -35,7 +35,7 @@ class MachineDriver
             } elseif ($requestedDrink === "coffee") {
                 return $drink->toCommand($request->extraHot);
             } elseif ($requestedDrink === "orangeJuice") {
-                return "O::";
+                return $drink->toCommand($request->extraHot);
             }
             $drink = "T";
             if ($request->extraHot) {
