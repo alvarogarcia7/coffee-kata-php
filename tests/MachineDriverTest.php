2048866a3c816bbbf2ac6235bd97f3edf5f8abb9
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 22:18:40 +0400

    Refactor: create tests for making drinks hot

--- tests/MachineDriverTest.php
+++ tests/MachineDriverTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Kata;
 
+use Kata\Drink;
 use Kata\FizzBuzz;
 use Kata\MachineDriver;
 use Kata\UserRequest;
@@ -26,12 +27,33 @@ class MachineDriverTest extends TestCase
         $this->assertEquals($expectedCommand, $this->machineDriver->process($userRequest));
     }
 
+    /** @test */
+    public function a_hot_drink_can_be_made_hot()
+    {
+        $drink = new Drink("Any", 0, true, "H::");
+
+        $actualCommand = $drink->toCommand(true);
+
+        $this->assertStringStartsWith("Hh:", $actualCommand);
+    }
+
+    /** @test */
+    public function a_non_hot_drink_cannot_be_made_hot()
+    {
+        $drink = new Drink("Any", 0, false, "H::");
+
+        $actualCommand = $drink->toCommand(true);
+
+        $this->assertStringStartsWith("H:", $actualCommand);
+    }
+
     private function process_user_requests_(): array
     {
         return [
             'With enough money, Drink maker makes 1 tea with 1 sugar and a stick' => [(new UserRequestBuilder())->tea()->withMoney(0.4)->withSugar()->build(), "T:1:0"],
             'With enough money, Drink maker makes 1 tea with 1 sugar and a stick, extra hot' => [(new UserRequestBuilder())->tea()->extraHot()->withMoney(0.4)->withSugar()->build(), "Th:1:0"],
             'Without enough money, Drink maker cannot make a tea' => [(new UserRequestBuilder())->tea()->withMoney(0.1)->withSugar()->build(), "M:missing-money:0.3"],
+
             'With enough money, Drink maker makes 1 chocolate with no sugar - and therefore no stick' => [(new UserRequestBuilder())->chocolate()->withMoney(0.6)->withoutSugar()->build(), "H::"],
             'With enough money, Drink maker makes 1 chocolate with no sugar (extra hot)' => [(new UserRequestBuilder())->chocolate()->extraHot()->withMoney(0.6)->withoutSugar()->build(), "Hh::"],
             'Without enough money, Drink maker cannot make a chocolate' => [(new UserRequestBuilder())->chocolate()->withMoney(0.3)->withoutSugar()->build(), "M:missing-money:0.2"],
