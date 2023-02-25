a596727909bed7dbf67fd6c8a5085ee8a52f4547
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-25 20:03:32 +0400

    Red: calculate total amount of money returned when the DrinkLog is not empty

--- tests/DrinkLogTest.php
+++ tests/DrinkLogTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Kata;
 
+use Kata\Drink;
 use Kata\DrinkLog;
 use PHPUnit\Framework\TestCase;
 
@@ -20,6 +21,17 @@ class DrinkLogTest extends TestCase
     public function by_default__the_total_is_zero()
     {
         $this->assertEquals(0, $this->drinkLog->totalAmountOfMoney());
+    }
+
+    /** @test */
+    public function with_drinks_inside__the_total_is_the_sum_of_their_prices()
+    {
+        $drink = new Drink("any", 0.1, false, "H");
+
+        $this->drinkLog->append(null, $drink);
+        $this->drinkLog->append(null, $drink);
+
+        $this->assertEquals(2 * 0.1, $this->drinkLog->totalAmountOfMoney());
     }
 
 }
