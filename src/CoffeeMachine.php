de5712e38ca2a6bf9fadcb8677b0912351310dac
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-03-16 10:21:28 +0400

    refactor: split choosing the drink from the sugar and stick

--- src/CoffeeMachine.php
+++ src/CoffeeMachine.php
@@ -9,20 +9,19 @@ class CoffeeMachine
     public function process(UserRequest $input)
     {
         $drinkCommand = "";
-        $sugarCommand = "";
+
         if ($input->drink === "message") {
             return "M:message-content";
         } elseif ($input->drink === "chocolate") {
             $drinkCommand = "H";
-            $sugarCommand = $input->sugar;
         } elseif ($input->drink === "coffee") {
             $drinkCommand = "C";
-            $sugarCommand = "2";
         } elseif ($input->drink === "tea") {
             $drinkCommand = "T";
-            $sugarCommand = "1";
         }
 
+        $stickCommand = "";
+        $sugarCommand = $input->sugar;
         if ("" != $drinkCommand) {
             $stickCommand = "";
             if ($sugarCommand === 0) {
@@ -30,10 +29,8 @@ class CoffeeMachine
             } else {
                 $stickCommand = "0";
             }
-            return implode(":", [$drinkCommand, $sugarCommand, $stickCommand]);
         }
 
-
-        throw new \Exception("This request is not supported: $input");
+        return implode(":", [$drinkCommand, $sugarCommand, $stickCommand]);
     }
 }
